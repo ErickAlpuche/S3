64e40f6c84c27127f40b3075f19765136645091d
Bob <your new email>
2022-12-16 16:49:39 -0500

    first commit

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+@section('title', $viewData["title"])
+@section('content')
+
+<div class="row">
+    <div class="col-md-6 col-lg-4 mb-2">
+        <img src="https://awsbucketerick.s3.amazonaws.com/public/img/game.png" class="img-fluid rounded">
+    </div>
+    
+    <div class="col-md-6 col-lg-4 mb-2">
+        <img src="https://awsbucketerick.s3.amazonaws.com/public/img/safe.png" class="img-fluid rounded">
+    </div>
+
+    <div class="col-md-6 col-lg-4 mb-2">
+        <img src="https://awsbucketerick.s3.amazonaws.com/public/img/submarine.png" class="img-fluid rounded">
+    </div>
+</div>
+@endsection
